609c6d40bf5e260f2da7c6dc607e41aa1ca79954
nainglinnphyo <nainglinnphyo24@gmail.com>
2025-01-09 16:07:59 +0630

    feat(app): x

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export const BACK_END_URL = "http://htoo-express.buclib.club/api/v1/";
+export const BACK_END_URL = "https://htoo-express.buclib.club/api/v1/";
 export const DISABLE_PERMISSION = true;
 // process.env.NEXT_PUBLIC_API_URL;
